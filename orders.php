3af2f59fff277d89dfe08c29f5da01ad5bebffc7
vaibhavpan12 <vaibhavpan7558@gmail.com>
2024-06-25 10:07:41 +0530

    Add files via upload

---
+++ orders.php
@@ -0,0 +1,92 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+} else {
+   $user_id = '';
+}
+
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_order = $conn->prepare("DELETE FROM `orders` WHERE id = ? AND user_id = ?");
+   $delete_order->execute([$delete_id, $user_id]);
+   header('location:orders.php');
+   exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Orders</title>
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="orders">
+
+   <h1 class="heading">Placed Orders.</h1>
+
+   <div class="box-container">
+
+   <?php
+   
+      if($user_id == ''){
+         echo '<p class="empty">Please login to see your orders</p>';
+      } else {
+         $select_orders = $conn->prepare("SELECT * FROM `orders` WHERE user_id = ?");
+         $select_orders->execute([$user_id]);
+         if($select_orders->rowCount() > 0){
+            while($fetch_orders = $select_orders->fetch(PDO::FETCH_ASSOC)){
+   ?>
+   
+   <div class="box">
+      <p>Placed on: <span><?= $fetch_orders['placed_on']; ?></span></p>
+      <p>Name: <span><?= $fetch_orders['name']; ?></span></p>
+      <p>Email: <span><?= $fetch_orders['email']; ?></span></p>
+      <p>Phone Number: <span><?= $fetch_orders['number']; ?></span></p>
+      <p>Address: <span><?= $fetch_orders['address']; ?></span></p>
+      <p>Payment Method: <span><?= $fetch_orders['method']; ?></span></p>
+      <p>Your orders: <span><?= $fetch_orders['total_products']; ?></span></p>
+      <p>Total price: <span>Nrs.<?= $fetch_orders['total_price']; ?>/-</span></p>
+      <p>Payment status: <span style="color:<?php if($fetch_orders['payment_status'] == 'pending'){ echo 'red'; } else { echo 'green'; }; ?>"><?= $fetch_orders['payment_status']; ?></span></p>
+
+      <?php if ($fetch_orders['payment_status'] != 'completed') { ?>
+          <p><a style="color: white; background-color: red;" href="orders.php?delete=<?= $fetch_orders['id']; ?>" onclick="return confirm('Are you sure you want to delete this order?');">Cancel Order</a></p>
+      <?php } ?>
+   </div>
+   <?php
+         }
+      } else {
+         echo '<p class="empty">No orders placed yet!</p>';
+      }
+   }
+   ?>
+
+   </div>
+
+</section>
+
+<?php include 'components/footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
